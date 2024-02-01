26e361ef860646b1428f1c252ea9b695ad039c93
Christopher Sandoval Terry <Chrisandoterry64@gmail.com>
2024-02-01 11:54:06 -0600

    Fun project

---
+++ NumberGuess/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace NumberGuess
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int winNum = r.Next(0,100);
+
+            bool win = false;
+
+            do
+            {
+                Console.Write("Guess a number inbetween 0-100.");
+                string s = Console.ReadLine();
+                int i = int.Parse(s);
+
+                if(i > winNum)
+                {
+                    Console.WriteLine("Guess lower");
+                }
+                else if(i < winNum)
+                {
+                    Console.WriteLine("Guess Higher");
+                }
+                else if(i == winNum)
+                {
+                    Console.WriteLine("YOU WIN !!");
+                    win = true;
+                }
+            }while(win == false);
+            
+
+            
+            
+        }
+
+    }
+
+}
